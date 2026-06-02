e976554b485d2abe41e1ea0a1657acc352671e80
Predrag Peharda <pehardap@gmail.com>
2026-06-02 13:03:49 +0200

    implement residual connections

--- app/bigram_v2.py
+++ app/bigram_v2.py
@@ -89,9 +89,12 @@ class MultiHeadAttention(nn.Module):
     def __init__(self, num_heads, head_size):
         super().__init__()
         self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.projection = nn.Linear(n_embd, n_embd)
 
     def forward(self, x):
-        return torch.cat([h(x) for h in self.heads], dim=-1)
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.projection(out)
+        return out
 
 # simple Linear layer followed by non-linearity
 class FeedForward(nn.Module):
@@ -99,8 +102,9 @@ class FeedForward(nn.Module):
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
         )
     
     def forward(self, x):
@@ -117,8 +121,8 @@ class Block(nn.Module):
         self.feed_forward = FeedForward(n_embd)
 
     def forward(self, x):
-        x = self.self_attention(x)
-        x = self.feed_forward(x)
+        x = x + self.self_attention(x)
+        x = x + self.feed_forward(x)
         return x
 
 # simple bigram model
